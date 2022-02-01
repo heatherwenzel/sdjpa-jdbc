2cccf2423e21792479a8601e243d23840f7f95f2
Heather Wenzel <heather.wenzel@ge.com>
2022-02-01 09:01:10 -0500

    Switch to using prepared statement

--- src/main/java/com/heatherwenzel/springdatademo/dao/AuthorDaoImpl.java
+++ src/main/java/com/heatherwenzel/springdatademo/dao/AuthorDaoImpl.java
@@ -4,10 +4,7 @@ import com.heatherwenzel.springdatademo.domain.Author;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao {
@@ -21,13 +18,14 @@ public class AuthorDaoImpl implements AuthorDao {
     @Override
     public Author getById(Long id) {
         Connection connection = null;
-        Statement statement = null;
+        PreparedStatement ps = null;
         ResultSet resultSet = null;
 
         try {
             connection = source.getConnection();
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM author where id = " + id);
+            ps = connection.prepareStatement("SELECT * FROM author where id = ?");
+            ps.setLong(1, id);
+            resultSet = ps.executeQuery();
 
             if (resultSet.next()) {
                 Author author = new Author();
@@ -45,8 +43,8 @@ public class AuthorDaoImpl implements AuthorDao {
                     resultSet.close();
                 }
 
-                if (statement != null){
-                    statement.close();
+                if (ps != null){
+                    ps.close();
                 }
 
                 if (connection != null){
